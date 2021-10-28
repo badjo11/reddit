5ba7118d61a61259b12c4d4e574aab10a4d70623
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-10-28 19:10:58 +0600

    basic auth done?

--- src/contexts/MainContext.js
+++ src/contexts/MainContext.js
@@ -1,19 +1,22 @@
 import React, { useReducer } from "react";
 import axios from "axios";
 export const mainContext = React.createContext();
-const API = "http://localhost:8000/countries/";
+const API = "http://localhost:8000/users/";
 
 const INIT_STATE = {
-  posts: [],
+  user: null,
+  failedLogin: null,
   //countryToEdit: null,
 };
 
 const reducer = (state = INIT_STATE, action) => {
   switch (action.type) {
-    case "GET_POSTES":
-      return { ...state, posts: action.payload };
-    //   case "GET_COUNTRY_TO_EDIT":
-    //     return { ...state, countryToEdit: action.payload };
+    case "LOGIN_USER":
+      return { ...state, user: action.payload };
+    case "FAILED_LOGIN":
+      return { ...state, failedLogin: action.payload };
+    case "LOGOUT_USER":
+      return { ...state, user: action.payload };
     default:
       return state;
   }
@@ -22,67 +25,68 @@ const reducer = (state = INIT_STATE, action) => {
 const MainContextProvider = (props) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
-  //   const getCountries = async (country) => {
-  //     try {
-  //       let res = await axios(API);
-  //       dispatch({
-  //         type: "GET_COUNTRIES",
-  //         payload: res.data,
-  //       });
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
+  const signUpUser = async (username, password) => {
+    try {
+      let res = await axios(API);
+      let user = res.data.find((user) => user.username === username);
+      if (user === undefined) {
+        try {
+          let { data } = await axios.post(API, { username, password });
+          dispatch({
+            type: "LOGIN_USER",
+            payload: data,
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  //   const editCountryPut = async (country) => {
-  //     try {
-  //       let res = await axios.put(API + country.id, country);
-  //       dispatch({
-  //         type: "GET_COUNTRY_TO_EDIT",
-  //         payload: null,
-  //       });
-  //       getCountries();
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
+  const loginUser = async (username, password) => {
+    try {
+      let res = await axios(API);
+      let user = res.data.find((user) => user.username === username);
+      let bool = false;
+      if (user) {
+        bool = user.password === password ? true : false;
+      }
+      if (bool) {
+        dispatch({
+          type: "LOGIN_USER",
+          payload: user,
+        });
+      } else {
+        dispatch({
+          type: "FAILED_LOGIN",
+          payload: true,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  //   const deleteCountry = async (id) => {
-  //     try {
-  //       let res = await axios.delete(API + id);
-  //       dispatch({
-  //         type: "GET_COUNTRY_TO_EDIT",
-  //         payload: null,
-  //       });
-  //       getCountries();
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-
-  //   const getCountryToEdit = async (id) => {
-  //     try {
-  //       let res = await axios(API + id);
-  //       dispatch({
-  //         type: "GET_COUNTRY_TO_EDIT",
-  //         payload: res.data,
-  //       });
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  //   };
-
-  //   const addCountry = async (country) => {
-  //     try {
-  //       let res = await axios.post(API, country);
-  //       getCountries();
-  //     } catch (e) {
-  //       console.log(e);
-  //     }
-  // };
+  const logoutUser = () => {
+    dispatch({
+      type: "LOGOUT_USER",
+      payload: null,
+    });
+  };
 
   return (
-    <mainContext.Provider value={{}}>{props.children}</mainContext.Provider>
+    <mainContext.Provider
+      value={{
+        signUpUser,
+        loginUser,
+        logoutUser,
+        state,
+      }}
+    >
+      {props.children}
+    </mainContext.Provider>
   );
 };
 
