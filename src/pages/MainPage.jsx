028ca1828faf4479649de03c66f8c4167a3518ea
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-10-29 11:16:34 +0600

    main page updates

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,16 @@
 import React, { useContext, useEffect } from "react";
 import Navibar from "../components/Navibar";
+import MainPS from "../components/pageScructure/mainPage/MainPS";
 
 const MainPage = () => {
   useEffect(() => {}, []);
 
-  return <Navibar />;
+  return (
+    <>
+      <Navibar />
+      <MainPS></MainPS>
+    </>
+  );
 };
 
 export default MainPage;
