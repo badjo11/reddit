7bdb90bb57db016fdaf4437aadcfb4c722b0f7bf
Dayana Dzhanuzakova <Dayana@Dayanas-MacBook.local>
2021-11-05 14:36:34 +0600

    fixed navigation issues

--- src/components/comments/SubheaderComments.jsx
+++ src/components/comments/SubheaderComments.jsx
@@ -2,23 +2,43 @@ import React, { useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { roomsContext } from "../../contexts/RoomsContext";
 import "./comments.css";
+import { useNavigate } from "react-router-dom"
 
 const SubheaderComments = () => {
   const { specificRoom } = useContext(roomsContext);
   console.log(specificRoom);
   const hz = useParams();
 
-  // useEffect(() => {
+  const navigate = useNavigate()
 
-  // }, [specificRoom])
   return (
     <div className="commentsSubHeader">
       <h2>{hz.roomtitle}</h2>
-      <Link to={"/r/" + hz.roomtitle}>
-        <button style={{ border: 0, backgroundColor: "darkgray" }}>X</button>
-      </Link>
+      <button onClick={() => navigate(-1)} style={{ border: 0, backgroundColor: "darkgray" }}>X</button>
     </div>
   );
 };
 
 export default SubheaderComments;
+
+
+
+//   return (
+//     <>
+//       <button onClick={() => navigate(-2)}>
+//         Go 2 pages back
+//       </button>
+//       <button onClick={() => navigate(-1)}>Go back</button>
+//       <button onClick={() => navigate(1)}>
+//         Go forward
+//       </button>
+//       <button onClick={() => navigate(2)}>
+//         Go 2 pages forward
+//       </button>
+//     </>
+//   );
+// }
+
+// <Link to={"/r/" + hz.roomtitle}>
+//   <button style={{ border: 0, backgroundColor: "darkgray" }}>X</button>
+// </Link>
