f4e242e64b6626c2beefc90cf563202d22c93eda
Atai Chynaliev <chynaliev.ae@gmail.com>
2021-11-08 18:26:38 +0600

    post creation

--- src/components/general/CreatePost.jsx
+++ src/components/general/CreatePost.jsx
@@ -3,33 +3,75 @@ import "./general.css";
 import iMG from "../../images/userdef.png";
 import { Button, Form, Modal } from "react-bootstrap";
 import { postsContext } from "../../contexts/PostsContext";
+import { useParams } from "react-router";
 
 const CreatePost = ({ roomtitle }) => {
   const { createPost } = useContext(postsContext);
   const [show, setShow] = useState(false);
-  // const { roomtitle } = useParams();
+  const linkParams = useParams();
 
   const [post, setPost] = useState({
-    postName: "",
     postText: "",
+    postImgURL: "",
   });
+
+  const [roomTitle, setRoomTitle] = useState("");
+
+  let user = localStorage.getItem("user");
+  user = JSON.parse(user);
 
   function handleChange(e) {
     let tempPost = { ...post, [e.target.name]: e.target.value };
     setPost(tempPost);
   }
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    if (user) {
+      setShow(true);
+    } else {
+      alert("Please login or sign up to make posts");
+    }
+  };
 
   function handleCreate(e) {
-    let user = localStorage.getItem("user");
-    user = JSON.parse(user);
     let time = new Date();
-    console.log(time);
     let timeMls = Date.now();
-    // console.log(timeMls)
-    createPost(post, user, time, roomtitle, timeMls);
-    handleClose();
+    if (roomtitle) {
+      createPost(post, user, time, roomtitle, timeMls);
+      handleClose();
+    } else {
+      if (roomTitle === "") {
+        console.log("here");
+        setRoomTitle(user.roomTitles[0]);
+      }
+      setTimeout(() => {
+        createPost(post, user, time, roomTitle, timeMls);
+      }, 100);
+      handleClose();
+    }
+  }
+
+  let selectRoom = <></>;
+
+  if (!linkParams.roomtitle && user) {
+    //setRoomTitle(
+    selectRoom = (
+      <Form.Group className="mb-3">
+        <Form.Label>Select a room you want to post in</Form.Label>
+        <Form.Select
+          aria-label="Default select example"
+          name="roomtitle"
+          onChange={(e) => setRoomTitle(e.target.value)}
+          value={roomTitle}
+        >
+          {user.roomTitles.map((item) => (
+            <option key={item + 1} value={item}>
+              {item}
+            </option>
+          ))}
+        </Form.Select>
+      </Form.Group>
+    );
   }
 
   return (
@@ -47,25 +89,26 @@ const CreatePost = ({ roomtitle }) => {
         </Modal.Header>
         <Modal.Body>
           <Form>
+            {selectRoom}
             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Post name</Form.Label>
+              <Form.Label>Your post</Form.Label>
               <Form.Control
                 onChange={handleChange}
-                name="postName"
+                name="postText"
                 type="text"
-                placeholder="post name"
+                placeholder="post"
               />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
-              <Form.Label>Text</Form.Label>
+              <Form.Label>Image URL</Form.Label>
               <Form.Control
                 onChange={handleChange}
-                name="postText"
-                as="textarea"
-                rows={10}
+                name="postImgURL"
+                type="text"
+                placeholder="image URL for your post"
               />
             </Form.Group>
           </Form>
