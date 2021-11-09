2145d1b782eaf98b330f9e0a6624a6d1a95a8bcc
Beknazar Omurbek <beknazaromurbek@gmail.com>
2021-11-09 17:59:30 +0600

    comments render by sort

--- src/components/comments/CommentsView.jsx
+++ src/components/comments/CommentsView.jsx
@@ -1,19 +1,28 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { commentsContext } from "../../contexts/CommentsContext";
 import Comment from "./Comment";
 const CommentsView = (props) => {
   const { commentsForPost, getCommentsForRoom } = useContext(commentsContext);
   // console.log(props.specificPost);
+  const [comm, setComm] = useState(commentsForPost);
   useEffect(() => {
     getCommentsForRoom(props.specificPost.id);
+
   }, []);
+
+  useEffect(() => {
+    let arr = [...commentsForPost];
+    arr.sort((a, b) => b.createdAtMs - a.createdAtMs);
+    setComm(arr);
+  }, [commentsForPost]);
+
   return (
     <>
       <div className="headings d-flex align-items-center mb-3">
         <h5>Comments({commentsForPost.length})</h5>
       </div>
       <div style={{ backgroundColor: "#f2f2f2" }}>
-        {commentsForPost.reverse().map((item) => (
+        {comm.map((item) => (
           <Comment key={item.id} val={item} />
         ))}
       </div>
